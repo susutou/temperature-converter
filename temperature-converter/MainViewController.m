fda6fecef9d20baca281de28778878c33002ca32
Susen Zhao <prcstyler@gmail.com>
2013-07-28 16:12:15 -0700

    main functionality added

--- temperature-converter/MainViewController.m
+++ temperature-converter/MainViewController.m
@@ -10,6 +10,9 @@
 
 @interface MainViewController ()
 
+- (void)updateValues;
+- (void)updateLabels;
+
 @end
 
 @implementation MainViewController
@@ -26,13 +29,94 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.srcTextField.delegate = (id)self;
+    
+    self.modeSegmentedControl.selectedSegmentIndex = 0;
+    [self.convertButton addTarget:self action:@selector(updateValues) forControlEvents:UIControlEventTouchUpInside];
+    [self.modeSegmentedControl addTarget:self action:@selector(updateLabels) forControlEvents:UIControlEventValueChanged];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)didReceiveMemoryWarnin
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITextField delegate
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(onDoneButton)];
+    
+    return YES;
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
+    self.navigationItem.rightBarButtonItem = nil;
+//    [self updateValues];
+    
+    return YES;
+}
+
+#pragma mark - Public methods
+
+- (void)onDoneButton {
+    [self.view endEditing:YES];
+    if ([self.srcTextField.text length] == 0) {
+        self.srcTextField.text = [NSString stringWithFormat:@"%0.2f", 0.0];
+    }
+    [self updateValues];
+}
+
+- (void)updateLabels {
+    switch (self.modeSegmentedControl.selectedSegmentIndex) {
+        case 0:
+            // C to F
+            self.srcLabel.text = @"Celsius (°C)";
+            self.destLabel.text = @"Fahrenheit (°F)";
+            break;
+        case 1:
+            // F to C
+            self.destLabel.text = @"Celsius (°C)";
+            self.srcLabel.text = @"Fahrenheit (°F)";
+            break;
+        default:
+            break;
+    }
+    [self updateValues];
+}
+
+- (void)updateValues {
+    
+    float celsius = 0.0;
+    float fahrenheit = 0.0;
+    
+    switch (self.modeSegmentedControl.selectedSegmentIndex) {
+        case 0:
+            // C to F
+            celsius = [self.srcTextField.text floatValue];
+            if (celsius < -273.15) {
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Freezing Cold!" message:@"Please input a valid Celsius temperature." delegate:nil cancelButtonTitle:@"Got it" otherButtonTitles:nil, nil];
+                [alert show];
+            } else {
+                fahrenheit = celsius * 1.8 + 32.0;
+                self.destTextField.text = [NSString stringWithFormat:@"%0.2f", fahrenheit];
+            }
+            break;
+        case 1:
+            // F to C
+            fahrenheit = [self.srcTextField.text floatValue];
+            if (fahrenheit < -459.67) {
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Freezing Cold!" message:@"Please input a valid Fahrenheit temperatre." delegate:nil cancelButtonTitle:@"Got it" otherButtonTitles:nil, nil];
+                [alert show];
+            } else {
+                celsius = (fahrenheit - 32.0) / 1.8;
+                self.destTextField.text = [NSString stringWithFormat:@"%0.2f", celsius];
+            }
+            break;
+        default:
+            break;
+    }
 }
 
 @end
