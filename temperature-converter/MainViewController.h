7d3bc8ac2433840ee57b714aea14b1ebd9ee3d6e
Susen Zhao <prcstyler@gmail.com>
2013-07-28 16:10:23 -0700

    property variables added

--- temperature-converter/MainViewController.h
+++ temperature-converter/MainViewController.h
@@ -10,4 +10,13 @@
 
 @interface MainViewController : UIViewController
 
+@property (nonatomic, weak) IBOutlet UILabel *srcLabel;
+@property (nonatomic, weak) IBOutlet UILabel *destLabel;
+@property (nonatomic, weak) IBOutlet UITextField *srcTextField;
+@property (nonatomic, weak) IBOutlet UITextField *destTextField;
+@property (nonatomic, weak) IBOutlet UIButton *convertButton;
+@property (nonatomic, weak) IBOutlet UISegmentedControl *modeSegmentedControl;
+
+- (IBAction)onDoneButton;
+
 @end
